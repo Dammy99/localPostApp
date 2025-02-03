6ee4b4c2be44cdb2f0b79bb95c4628ba42400f8e
DESKTOP/Nazar <DESKTOP/Nazar>
2025-02-03 20:43:40 +0200

    +

--- LocalPostAppFrontEnd/vite.config.ts
+++ LocalPostAppFrontEnd/vite.config.ts
@@ -5,13 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:5022',
+        target: 'https://localhost:5022',  
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path,
-        protocolRewrite: 'http'
+        secure: false
       }
     }
   }
